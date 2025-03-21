66ebfe8691f8e245e022e6f58eace50c677bd249
theabhishek <callabhimishra@gmail.com>
2025-03-21 13:51:25 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,10 +1,8 @@
-import './config/instrument.js'
+
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/db.js'
-
-const Sentry = require("@sentry/node");
 import { clerkWebhooks } from './controllers/webhooks.js'
 import companyRoutes from './routes/companyRoutes.js'
 import connectCloudinary from './config/cloudinary.js'
@@ -27,9 +25,6 @@ app.use(clerkMiddleware())
 
 // Routes
 app.get('/', (req, res) => res.send("API Working"))
-app.get("/debug-sentry", function mainHandler(req, res) {
-  throw new Error("My first Sentry error!");
-});
 app.post('/webhooks', clerkWebhooks)
 app.use('/api/company', companyRoutes)
 app.use('/api/jobs', jobRoutes)
@@ -38,7 +33,7 @@ app.use('/api/users', userRoutes)
 // Port
 const PORT = process.env.PORT || 5000
 
-Sentry.setupExpressErrorHandler(app);
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
