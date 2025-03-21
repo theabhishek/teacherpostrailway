ba3f367d39499c3e8633ed5437814cea9b6d2ead
Abhishek Mishra <ab.mishra@yahoo.com>
2025-03-21 12:56:45 +0530

    Update instrument.js

--- server/config/instrument.js
+++ server/config/instrument.js
@@ -1,27 +1,19 @@
 // Import with `import * as Sentry from "@sentry/node"` if you are using ESM
-import * as Sentry from "@sentry/node"
-import { nodeProfilingIntegration } from "@sentry/profiling-node";
-
+//https://d82a7f34168d4f9019200931608b098f@o4509008920707073.ingest.us.sentry.io/4509008929554432
+// Ensure to call this before requiring any other modules!
 Sentry.init({
   dsn: "https://d82a7f34168d4f9019200931608b098f@o4509008920707073.ingest.us.sentry.io/4509008929554432",
   integrations: [
+    // Add our Profiling integration
     nodeProfilingIntegration(),
-    Sentry.mongooseIntegration()
   ],
-  // Tracing
-  tracesSampleRate: 1.0, //  Capture 100% of the transactions
-});
-// Manually call startProfiler and stopProfiler
-// to profile the code in between
-Sentry.profiler.startProfiler();
 
-// Starts a transaction that will also be profiled
-Sentry.startSpan({
-  name: "My First Transaction",
-}, () => {
-  // the code executing inside the transaction will be wrapped in a span and profiled
-});
+  // Add Tracing by setting tracesSampleRate
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
 
-// Calls to stopProfiling are optional - if you don't stop the profiler, it will keep profiling
-// your application until the process exits or stopProfiling is called.
+  // Set sampling rate for profiling
+  // This is relative to tracesSampleRate
+  profilesSampleRate: 1.0,
+});
 Sentry.profiler.stopProfiler();
